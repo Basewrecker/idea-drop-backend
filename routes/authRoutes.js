d28fd1371cb2ce949f16c9170f2a49873eab49e5
Basewrecker <siddhunrs@gmail.com>
2026-05-01 13:34:03 +0530

    Refresh token  and decrypt added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
+import { jwtVerify } from "jose";
+import { JWT_SECRET } from "../utils/getJwtSecret.js";
 import { generateToken } from "../utils/generateToken.js";
 const router = express.Router();
 
@@ -51,6 +53,56 @@ router.post("/register", async (req, res, next) => {
   }
 });
 
+// @route POST ap/auth/register
+// @description Register new User
+// @access Public
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("Email and password are required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(401);
+      throw new Erorr("Invalid credentials");
+    }
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      res.status(401);
+      throw new Error("Password is incorrect");
+    }
+
+    const payload = { userId: user._id.toString() };
+    const accessToken = await generateToken(payload, "1m");
+    const refreshToken = await generateToken(payload, "30d");
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+
+    res.status(201).json({
+      accessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
 // @route POST ap/auth/logout
 // @description Logout User
 // @access Private
@@ -63,6 +115,46 @@ router.post("/logout", (req, res) => {
   });
 
   res.status(200).json({ message: "Logged out the user" });
+});
+
+// @route POST ap/auth/register
+// @description Register new User
+// @access Public (Needs valid token)
+
+router.post("/refresh", async (req, res, next) => {
+  try {
+    const token = req.cookies?.refreshToken;
+    console.log("Refreshing token..");
+
+    if (!token) {
+      res.status(401);
+      throw new Error("No refresh token");
+    }
+
+    const { payload } = await jwtVerify(token, JWT_SECRET);
+
+    const user = await User.findById(payload.userId);
+    if (!user) {
+      res.status(401);
+      throw new Error("No user found");
+    }
+
+    const newAccessToken = await generateToken(
+      { userId: user._id.toString() },
+      "1m",
+    );
+    res.json({
+      accessToken: newAccessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    res.status(401);
+    next(err);
+  }
 });
 
 export default router;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import  dotenv  from 'dotenv';
-import ideaRouter from './routes/ideaRoutes.js';
-import authRouter from './routes/authRoutes.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import connectDB from './config/db.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import ideaRouter from "./routes/ideaRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -14,26 +15,26 @@ const PORT = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/ideas', ideaRouter);
-app.use('/api/auth', authRouter);
+app.use(cookieParser());
+app.use("/api/ideas", ideaRouter);
+app.use("/api/auth", authRouter);
 
-app.use((req,res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error)
-})
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
 
-
-app.use(errorHandler)
-app.get('/', (req, res) => {
-    res.json({ message: 'Idea Drop API is running' });
+app.use(errorHandler);
+app.get("/", (req, res) => {
+  res.json({ message: "Idea Drop API is running" });
 });
 
 const startServer = async () => {
-    await connectDB();
-    app.listen(PORT, () => {
-        console.log(`Server is running on ${PORT}`);
-    });
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
 };
 
 startServer();
